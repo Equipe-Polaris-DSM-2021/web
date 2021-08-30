70adc04b916c2a6cc542ad7247014cfb82b7d321
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-08-30 13:40:22 -0300

    Toggle Layers Control with Streetview, Satelliteview

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
-import {Map, TileLayer } from 'react-leaflet';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Map, TileLayer, Marker, Popup, LayersControl, LayerGroup, Circle, Rectangle, FeatureGroup } from 'react-leaflet';
 
 import "../styles/pages/home.css"
 
@@ -8,8 +10,22 @@ export default function Home() {
       <Map center={[-23.2683,-45.913486]}
         zoom={15} 
         style={{ width: '100%', height: '100%'}}
+        scrollWheelZoom={true}
       >
-        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />          
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="Street View">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Satellite View">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>      
       </Map>
     </div>
   )
