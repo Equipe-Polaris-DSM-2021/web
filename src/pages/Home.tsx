2f779c48559711816bc7a801e96060472e0d43b4
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-09-08 13:37:51 -0300

    Adicionado os textos e inicio da estrutura da Homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,13 +4,35 @@ import "../styles/pages/home.css"
 
 export default function Home() {
   return (
-    <div id="page-map">
-      <Map center={[-23.2683,-45.913486]}
-        zoom={15} 
-        style={{ width: '100%', height: '100%'}}
-      >
-        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />          
-      </Map>
+    <>
+    <div id="simples-imgs">
+    <h1>Um jeito simples e rápido de <br/>consumir imagens de satélites</h1>
+    <p>Com poucos cliques, acesse imagens de diversos satélites disponíveis, 
+      definindo as opções de visualização de acordo com sua preferência.</p>
+      <div id="side-map-img">
+        <img></img>
+      </div>
     </div>
+    <div id="satelites-disp">
+      <h1>Satélites disponíveis</h1>
+      <p>Disponibilizamos uma série de satélites para que você selecione 
+        de acordo com sua necessidade no momento. Veja abaixo as opções:</p>
+        <div>
+        Landsat 8
+        Sentinel 1 e 2
+        CBERS 4
+        CBERS 4A
+        Amazônia 1
+      </div>
+    </div>
+    <div id="Download-imgs">
+      <h1>Download de imagens selecionadas</h1>
+      <p>Selecionando a área de interesse, ainda é possível fazer o download 
+        da imagem da região de duas formas: como imagem bruta ou como imagem processada. 
+        Basta clicar no ícone da área de interesse, escolher uma das opções e fazer o download.</p>
+
+
+    </div>
+    </>
   )
 }
