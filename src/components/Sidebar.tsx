c1f80159658a5f8871b0a1aa1fe81dcbbfa959ad
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2021-09-14 16:01:03 -0300

    Add CircularProgress

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import { MdClear, MdDehaze } from "react-icons/md";
 import { FaMousePointer } from "react-icons/fa";
+import { CircularProgress } from "@material-ui/core"; // Bolinha de carregamento
 
 import { Context } from "../context/MapFilterContext";
 
@@ -26,6 +27,9 @@ export default function Sidebar() {
   const [endPeriodDate, setEndPeriodDate] = useState("");
 
   const [cloudRange, setCloudRange] = useState("50");
+
+  // Para controlar quando ativar a bolinha de carregamento
+  const [inSearch, setInSearch] = useState(false);
 
   const handleFilter = () => {
     const satelliteOptions = {
@@ -53,6 +57,8 @@ export default function Sidebar() {
     };
 
     performFilteredSearch(payload);
+
+    setInSearch(!inSearch); // ativa a bolinha de carregamento quando o botão for clicado
   };
 
   const handleCloseFilter = () => {
@@ -201,7 +207,19 @@ export default function Sidebar() {
         </section>
       </main>
       <footer>
-        <Button width="100%" onClick={handleFilter} children="Buscar" />
+        <Button
+          width="100%"
+          onClick={handleFilter}
+          children={
+            !inSearch ? (
+              // Se não for feita a pesquisa...
+              "Buscar"
+            ) : (
+              // Quando a pesquisa for feita...
+              <CircularProgress size="1.1rem" style={{ color: "#44AAFF" }} />
+            )
+          }
+        />
       </footer>
     </aside>
   );
